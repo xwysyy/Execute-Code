770309387ed34fd8ba89c089c708da4238683b01
xwysyy <3185205565@qq.com>
2024-12-22 13:53:05 +0800

    update

---
+++ README.md
@@ -0,0 +1,27 @@
+# 准备
+
+  * 需要安装jq，`sudo apt-get install jq`。
+  * 未启用安全沙箱测试，需要配置python和cpp的编译环境，也可自行构建docker镜像。
+  
+# 使用
+
+* 在`init/modules.yaml`中配置需要的大模型
+* 在`.env`中配置api_key
+* 请参照模版配置`data/data.csv`内容
+* 在`data/prompt.txt`中配置大模型生成代码的prompt。
+* 在`main.py`中，读取`data/data.csv`中的题目，并调用api生成代码，进行评测。
+* 假设题目名称为`test`，并进入到`data/problems/test`目录下。
+  * `problem.json`是题目测试的基本信息。
+  * `result.json`是测试结果。
+  * `cases`是测试用例。
+  * `code`是大模型生成的代码。
+  * `desc`是题目描述。
+  * `generate`用于生成测试用例以及测试代码。
+
+# 可能会出现的问题：
+
+* 返回的代码部分缺少换行符，导致无法编译。
+
+# todo
+
+* 对于同一题目同一接口多次询问返回的代码进行整理
---
+++ ask.py
@@ -0,0 +1,81 @@
+import os
+import yaml
+import concurrent.futures
+from openai import OpenAI
+from dotenv import load_dotenv
+from init import model_path, problem_path, data_path
+import re
+
+load_dotenv()
+with open(model_path, 'r', encoding='utf-8') as file:
+    models_config = yaml.load(file, Loader=yaml.FullLoader)
+models_list = [
+    model for model in list(models_config.keys()) 
+    if models_config[model].get('enabled', False) 
+]
+with open(f'{data_path}/prompt.txt', 'r', encoding='utf-8') as f:
+    prompt = f.read()
+
+def get_code(question: str, model: str, api_key: str, base_url: str):
+    """
+    调用指定大模型api生成指定问题的代码, question为问题描述, model为大模型名称
+    """
+    api_key = os.getenv(api_key)
+    if api_key is None:
+        raise ValueError('API Key not found')
+    client = OpenAI(api_key = api_key, base_url = base_url)
+    chat_completion = client.chat.completions.create(
+        messages = [
+            {"role": "system", "content": prompt},
+            {"role": "user","content": question}
+        ],
+        model = model,
+    )
+    
+    code = chat_completion.choices[0].message.content
+    code = '```cpp\nclass Solution' + code.split('class Solution')[-1] 
+    code = '/*\n\n' + code + '\n\n*/'
+    code = code.replace('```cpp', '*/\n')
+    code = code.replace('```', '\n/*')
+    # 把非数字字母下划线的字符替换成_
+    tem_name = re.sub(r'[^a-zA-Z0-9_]', '_', model).upper()  
+    code = f'#ifndef {tem_name}_H\n#define {tem_name}_H\n#include <bits/stdc++.h>\nusing namespace std;\n\n{code}\n\n#endif'
+
+    return code
+
+def ask(name: str, models_list: list = models_list):
+    """
+    调用所有启用的大模型api生成指定问题的代码, name为问题名称, models_list为大模型列表, 默认为启用的大模型列表, 可以指定模型
+    """
+    if not os.path.exists(problem_path):
+        os.makedirs(problem_path)
+    path = f'{problem_path}/{name}'
+    if not os.path.exists(path):
+        raise ValueError('Problem not found')
+    if not os.path.exists(f'{path}/desc/desc.txt'):
+        raise ValueError('Problem description not found')
+    with open(f'{path}/desc/desc.txt', 'r', encoding='utf-8') as f:
+        problem = f.read()
+    path = f'{path}/codes'
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    def save_code(model: str):
+        enaled = models_config[model]['enabled']
+        if not enaled:
+            return
+        api_key = models_config[model]['api_key']
+        if api_key is None:
+            raise ValueError('API Key not found')
+        base_url = models_config[model]['base_url']
+        cpp_code = get_code(problem, model, api_key, base_url)
+        with open(f'{path}/{model}.h', 'w', encoding='utf-8') as f:
+            # f.write(cpp_code)
+            for chunk in [cpp_code[i:i+1024] for i in range(0, len(cpp_code), 1024)]:
+                f.write(chunk)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(save_code, models_list)
+
+if __name__ == '__main__':
+    print(f'Enabled models: {models_list}') 
